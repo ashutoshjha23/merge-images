fb8c4cfce97b26b1d7c56f15c6c518b1b908364c
Ashutosh Jha <ashutoshjha2312@gmail.com>
2024-10-15 11:06:23 +0530

    traensparent backgroud

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -4,7 +4,7 @@ import html2canvas from 'html2canvas';
 const DownloadButton = () => {
     const handleDownload = async () => {
         const element = document.querySelector('.overlay-container');
-        const canvas = await html2canvas(element);
+        const canvas = await html2canvas(element, { backgroundColor: null });
         const dataURL = canvas.toDataURL('image/png');
         const link = document.createElement('a');
         link.href = dataURL;
